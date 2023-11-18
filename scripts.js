62e4366239bf1d677fdd7003e5fae470fab74a27
JuanGutierrez95 <juan.gutierrez19958@gmail.com>
2023-11-18 11:16:18 -0300

    trabajando en el archivo scripts.js

--- scripts.js
+++ scripts.js
@@ -2,14 +2,14 @@ document.addEventListener("DOMContentLoaded", function () {
   const selectSeguro = document.getElementById("tipodeseguro");
   const precioSeguro = document.getElementById("precioSeguro");
 
-  // Función para actualizar el precio del seguro
+  
   function actualizarPrecio() {
     const opcionSeleccionada = selectSeguro.options[selectSeguro.selectedIndex];
     const precio = opcionSeleccionada.getAttribute("data-precio");
     precioSeguro.textContent = `Precio del Seguro: $${precio}`;
   }
 
-  // Evento de cambio en el select
+  
   selectSeguro.addEventListener("change", actualizarPrecio);
 
   // Mostrar el precio inicial al cargar la página
